7784206f2794f155e76b607ea1e0e6d033357726
Vinny Meller <vinnymeller@gmail.com>
2022-08-27 20:01:14 -0500

    ch 9 error propagation

--- ch9/errors/src/main.rs
+++ ch9/errors/src/main.rs
@@ -1,4 +1,31 @@
 use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_concise() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
 
 
 fn main() {
@@ -6,6 +33,32 @@ fn main() {
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
     };
+
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    let greeting_file_2 = File::open("hello.txt").unwrap();
+    let notha_file = File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    let username = read_username_from_file().unwrap();
+    println!("{:?}", username);
+    let username = read_username_from_file_concise().unwrap();
+    println!("{:?}", username);
+
+
 }
