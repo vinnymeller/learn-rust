b048f3fc009ad9cc7005d8f2bc8ae05c305a1c0b
Vinny Meller <vinnymeller@gmail.com>
2022-08-27 19:35:19 -0500

    match to get result or error from Result

--- ch9/errors/src/main.rs
+++ ch9/errors/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::File;
+
+
 fn main() {
-    panic!("crash and burn");
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
